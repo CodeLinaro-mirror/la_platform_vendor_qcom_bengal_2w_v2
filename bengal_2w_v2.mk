c772984e8c90f66db0dee623be2ed8a7cc4a3e8a
Gopalarao Marthi <quic_gmarthi@quicinc.com>
2024-07-25 14:28:14 +0530

    Bengal_2w_v2: Properties to detect SIM for bengal_2w_v2
    Adding properties related to SIM hotswap
    
    Change-Id: I204cc5e7cdf8323393cb5b49f27fcc5a7b7ab10e
    Signed-off-by: Kripa Bhat <quic_kripbhat@quicinc.com>

--- bengal_2w_v2.mk
+++ bengal_2w_v2.mk
@@ -7,6 +7,10 @@ TARGET_BOARD_SUFFIX := _2w
 TARGET_BOARD_DERIVATIVE_SUFFIX := _v2
 DEVICE_MANIFEST_FILE := device/qcom/bengal_2w/manifest.xml
 
+# vendor properties to enable sim hotswap
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.radio.hotswap_enable = 1
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.radio.hotswap_polarity = 1
+
 # Kernel modules install path
 #KERNEL_MODULES_INSTALL := dlkm
 #KERNEL_MODULES_OUT := out/target/product/bengal_2w_v2/$(KERNEL_MODULES_INSTALL)/lib/modules
