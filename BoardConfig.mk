22eb02514c5d452f0dd718a7e392ebe1457b1f0f
Raja Kumar Grandhi <quic_rajagran@quicinc.com>
2024-09-23 19:40:12 +0530

    Updated BoardConfig.mk to fix bengal_2w_v2 HY11 errors
    Change-Id: I928d86e3b90e7bdcd3ecd0e53b7b002e39ade4ed

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,3 +3,6 @@ KERNEL_MODULES_INSTALL := dlkm
 KERNEL_MODULES_OUT := out/target/product/bengal_2w_v2/$(KERNEL_MODULES_INSTALL)/lib/modules
 
 include device/qcom/bengal_2w/BoardConfig.mk
+
+TARGET_ARCH := arm64
+TARGET_2ND_ARCH := arm
